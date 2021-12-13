73b9c32ad1d0b788bf1468ab9f9f933c1afa71fe
PhongXuanDN <phamxuanphong238@gmai.com>
2021-12-13 14:12:42 +0700

    update s4 s5 s6

---
+++ ss6.Javascript/myJS.js
@@ -0,0 +1,14 @@
+function showAlert() {
+    alert('Hello các bạn')
+}
+// function showPrompt() {
+//    var name = prompt('Bạn tên gì')
+//     alert('Chào bạn: '+ name)
+// }
+function showPrompt() {
+    var age = parseInt(prompt('Bạn tên gì'))
+    alert(age+1)
+}
+function showConfirm() {
+    confirm('Hello các bạn')
+}
