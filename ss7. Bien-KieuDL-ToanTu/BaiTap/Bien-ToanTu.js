0845dc876e1c494d76b34647e53bfeca25b57823
PhongXuanDN <phamxuanphong238@gmai.com>
2021-12-15 18:36:42 +0700

    update bap tap

--- ss7. Bien-KieuDL-ToanTu/BaiTap/Bien-ToanTu.js
+++
@@ -1,9 +0,0 @@
-function DiemTB() {
-    let DiemToan;
-    let DiemHoa;
-    let DiemSinh;
-    DiemToan = prompt('Nhap diem Toan')
-    DiemHoa = prompt('Nhap diem Hoa')
-    DiemSinh = prompt('Nhap diem Sinh')
-
-}
